e44622d17647b836cc8f2ee1e273c4fcd4b513c3
Harsh Govind <harsh5092001@gmail.com>
2025-10-26 23:02:46 +0530

    transaction card and others

--- components/ui/custom/Navbar.tsx
+++ components/ui/custom/Navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = ({ title }: { title: string }) => {
     const router = useRouter();
 
     return (
-        <div className="flex justify-between items-center py-4 select-none">
+        <div className="flex justify-between items-center py-4 select-none px-2 md:px-0">
             <h1 className="text-3xl font-bold">{title}</h1>
             <div className="flex items-center gap-2">
                 <ThemeToggle />
